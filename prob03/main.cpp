54ae0b55788b633558e79cd0e5bc699a0997dd95
ejaguirre <ejaguirre@cs104u-3c.acad.fullerton.edu>
2019-09-14 14:02:35 -0700

    included 2 constants and properly gives the sales prediction

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,24 @@
 // Sales prediction
+
+#include <iostream>
+
+int main()
+{
+  const int SALES_LAST = 2103419277; //amount of last years sales
+  const double SALES_PERC = .18; //percentage by which the sales have increased
+
+  unsigned int sales,nsales;
+
+  //amount of last years sales
+  std::cout<< "Last years's sales were " << SALES_LAST << std::endl;
+
+  //calculate amount of this years Sales
+  sales = SALES_LAST * SALES_PERC;
+
+  nsales = sales + SALES_LAST;
+
+  //amount of this years Sales
+  std::cout<< "This years's sales prediction: " << nsales << std::endl;
+
+  return 0;
+}
