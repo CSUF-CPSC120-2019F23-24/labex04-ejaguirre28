73d1a44ad195bc3d364bdbdda99741786f4a579d
ejaguirre <ejaguirre@cs104u-3c.acad.fullerton.edu>
2019-09-14 13:25:01 -0700

    added static cast and adding one to characters with comments explaining why

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -7,16 +7,16 @@ int main()
   // TODO#1: use the "static_cast" operator to cast the character to a short
   // then print both the char and the short
 
-  char my_char1 = 'J';
-  short my_short1 = ;
-  std::cout << "The ASCII value for " << my_char1 << " is " << my_short1 << std::endl;
+  char ch = 'J';
+  short my_short1 = 74;
+  std::cout << "The ASCII value for " << static_cast<char>(ch) << " is " << static_cast<short>(ch) << std::endl;
 
   std::cout << std::endl; // blank line to separate output from the different exercises
 
   // TODO#2: add one to the character to see what gets printed out
 
   char my_char2 = 'C';
-  my_char2 = ;
+  my_char2 = my_char2 + 1;
   std::cout << "Char1: " << my_char2 << std::endl;
 
   std::cout << std::endl; // blank line to separate output from the different exercises
@@ -26,7 +26,7 @@ int main()
   // longer alphabetic
 
   char my_char3 = 'z';
-  my_char3 = ;
+  my_char3 = my_char3 + 1; //The char is no longer alphabetic because the next ASCII character is {
   std::cout << "Char2: " << my_char3 << std::endl;
 
   std::cout << std::endl; // blank line to separate output from the different exercises
@@ -42,13 +42,13 @@ int main()
 
   std::cout << "Short (" << sizeof(my_short2) << " bytes): "
     << std::dec << my_short2 << std::hex << " (0x" << my_short2 << ')';
-  my_short2 = ;
+  my_short2 = my_short2 + 1; //Became negative because the max value for short is 32767 so it wraps to the min value
   std::cout << ", after adding 1: " << std::dec << my_short2
     << std::hex << " (0x" << my_short2 << ")\n";
 
   std::cout << "Int (" << sizeof(my_short2) << " bytes): "
     << std::dec << my_int2 << std::hex << " (0x" << my_int2 << ')';
-  my_int2 = ;
+  my_int2 = my_int2 + 1; //my_int2 did not become a negative is because it is an int which it's max value is much higher
   std::cout << ", after adding 1: " << std::dec << my_int2
     << std::hex << " (0x" << my_int2 << ")\n";
 
@@ -63,13 +63,13 @@ int main()
 
   std::cout << "Unsigned short (" << sizeof(my_short3) << " bytes): "
     << std::dec << my_short3 << std::hex << " (0x" << my_short3 << ')';
-  my_short3 = ;
+  my_short3 = my_short3 + 1; //Became 0 because the max value for unsigned short is 65535 so it wraps to the min value
   std::cout << ", after adding 1: " << std::dec << my_short3
     << std::hex << " (0x" << my_short3 << ")\n";
 
   std::cout << "Same value as int (" << sizeof(my_int3) << " bytes): "
     << std::dec << my_int3 << std::hex << " (0x" << my_int3 << ')';
-  my_int3 = ;
+  my_int3 = my_int3 + 1;//my_int3 did not become 0 because it is an int which it's max value is much higher
   std::cout << ", after adding 1: " << std::dec << my_int3
     << std::hex << " (0x" << my_int3 << ")\n";
 
